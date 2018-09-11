22cc5ed63905b417b5252fa828d346ea0c90ee8f
Adrian Chira <adrianchira@MacBook-Pro-3.local>
2018-09-11 22:57:24 +0300

    added plot2

--- plot2.R
+++ plot2.R
@@ -3,16 +3,22 @@ library(dplyr)
 library(magrittr)
 #read data file (the file was downloaded and unzipped in the working directory)
 df <-read.table("./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
-#Change date format
-df %<>%mutate(Date=as.Date(Date, format = "%d/%m/%Y"))
+#Concatenate Date and Time
+df$DT <- paste(df$Date,df$Time)
+#Change date format to select the sample
+df %<>%
+  + mutate(Date=as.Date(Date, format = "%d/%m/%Y"))
 #Getting the records for dates higher or equal to 2007-02-01 and lower or equal to 2007-02-02
 mydata <- subset(df, Date >= as.Date("2007-02-01"))
 mydata <- subset(mydata, Date <= as.Date("2007-02-02"))
 #free df from memory since we do not need it anymore
 rm(df)
+#Convert DT to POSIXct
+mydata %<>%mutate(DT=as.POSIXct(DT,format="%d/%m/%Y %H:%M:%S"))
 #open the png device with width and height params
-png(file="plot1.png",width=480,height=480)
-#make the histogram
-hist(mydata$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+png(file="plot2
+    .png",width=480,height=480)
+#make the line graph
+plot(mydata$DT, mydata$Global_active_power, typ = 'l', ylab = "Global Active Power (kilowatts)", xlab = "")
 #close the device
 dev.off()
